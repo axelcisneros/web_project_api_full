2e67b4c3e55ddfa0b7c1009c73deb4764c8efc59
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-03-26 18:35:32 -0600

    instal vlidator, update controllers and models

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const requiredMessage = 'El campo {PATH} es obligatorio.';
 const minlengthMessage = 'El campo {PATH} debe tener al menos {MINLENGTH} caracteres.';
 const maxlengthMessage = 'El campo {PATH} debe tener como máximo {MAXLENGTH} caracteres.';
 const urlMessage = 'El campo {PATH} debe ser una URL válida.';
+const emailMessage = 'El campo {PATH} debe ser un email válido.';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,9 +24,24 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, requiredMessage],
         validate: {
-            validator: (v) => /^(https?:\/\/)([\w\-_]+\.)+[\w\-_]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/.test(v),
+            validator: (v) => validator.isURL(v, { protocols: ['http', 'https'], require_protocol: true }),
             message: urlMessage
         }
+    },
+    email: {
+        type: String,
+        required: [true, requiredMessage],
+        unique: true,
+        validate: {
+            validator: (v) => validator.isEmail(v),
+            message: emailMessage
+        }
+    },
+    password: {
+        type: String,
+        required: [true, requiredMessage],
+        select: false
     }
 });
+
 module.exports = mongoose.model('user', userSchema);
